4c3fd7e465965307b15dd8f85b220a48c80004f7
Genos <cpwfe87@gmail.com>
2017-09-06 10:54:57 -0400

    Updated Base design

---
+++ blog_project/blog/static/blog/js/main.js
@@ -0,0 +1,13 @@
+// Javascript for Blog Project
+
+// Function for confirmation to external link
+
+function confirmLink() {
+	if (!confirm("You are about to leave this page and will be redirected to an external link that is a valid link to a personal page on a social website.\nIf this is what you wanted to do, click 'OK'.\nIf this is not what you wanted to do, click 'Cancel'.")) {
+		window.location='index.html';
+	}
+	else {
+		
+	}
+
+}
